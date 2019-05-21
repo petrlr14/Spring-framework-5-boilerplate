8a10cdc277e1bb90f0527ce2a0e6762c2249e9e3
petrlr14 <pedrodeveloper14@gmail.com>
2019-05-21 15:40:41 -0600

    rip tarea 6

---
+++ WebContent/WEB-INF/views/create.jsp
@@ -0,0 +1,20 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<form:form>
+		<label>Nombre</label>
+		<input type="text">
+		<label>Gómez</label>
+		<input type="text">
+		<label>Edad</label>
+		<input type="text">
+	</form:form>
+</body>
+</html>
--- src/com/uca/capas/domain/Student.java
+++ src/com/uca/capas/domain/Student.java
@@ -2,7 +2,10 @@ package com.uca.capas.domain;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 @Entity
@@ -10,6 +13,8 @@ import javax.persistence.Table;
 public class Student {
 	
 	@Id
+	@GeneratedValue(generator="student_id_student_seq", strategy = GenerationType.AUTO)
+	@SequenceGenerator(name="student_id_student_seq", sequenceName = "public.student_id_student_seq", allocationSize = 1)
 	@Column(name="id_student")
 	private Integer cStudent;
 	
